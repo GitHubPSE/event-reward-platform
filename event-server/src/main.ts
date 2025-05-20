af364f5dfaea78bde7a3c03d056ff189d3ef4391
GitHubPSE <seee0758@gmail.com>
2025-05-20 15:00:10 +0900

    헤더 없는 경우 처리

--- event-server/src/main.ts
+++ event-server/src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use((req, res, next) => {
-    const isFromGateway =
-      req.headers['x-from-gateway'].toLowerCase() === 'true';
-    if (!isFromGateway) {
+   const xFromGateway = req.headers['x-from-gateway'];
+
+    if (!xFromGateway || xFromGateway.toString().toLowerCase() !== 'true') {
       return res.status(403).json({
-        message: 'event-server는 gateway를 통해서만 접근 가능합니다.',
+        message: '비정상 접근입니다.',
       });
     }
     next();
